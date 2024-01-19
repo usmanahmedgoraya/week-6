3b47a25a9e7d7f033f6a0f9a74a0236f48f97fe7
usmanahmedgoraya <usman_ahmed_intern@netixsol.com>
2024-01-19 11:18:31 +0500

    Add More admins

--- Day-1,2/Promptopia/app/api/prompt/route.js
+++ Day-1,2/Promptopia/app/api/prompt/route.js
@@ -5,7 +5,7 @@ import user from "@models/users.model";
 export const GET = async (request) => {
     try {
         await connectToDB()
-        const prompts = await Prompt.find({}).populate('creator');
+        const prompts = await Prompt.find().populate('creator');
         return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status: 500 })
--- Day-3,4,5/Ecommerce/admin/pages/api/auth/[...nextauth].js
+++ Day-3,4,5/Ecommerce/admin/pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 
-const adminEmails = ['gorayausman061@gmail.com'];
+const adminEmails = ['gorayausman061@gmail.com','rana.habib313@gmail.com','usmanahmedkharal@gmail.com'];
 
 export const authOptions = {
   secret: process.env.SECRET,
